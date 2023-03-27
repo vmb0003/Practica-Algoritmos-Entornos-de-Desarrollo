f3f7f6f8a34056ebaf92e559b8bb8faad4c51d4e
Victor <vmb0003@alu.medac.es>
2023-03-27 09:51:33 +0200

    Creacion metodo factorial

--- com.entornos.victormoreno.algoritmos/src/com/entornos/victormoreno/algoritmos/algoritmos.java
+++ com.entornos.victormoreno.algoritmos/src/com/entornos/victormoreno/algoritmos/algoritmos.java
@@ -1,14 +1,30 @@
 package com.entornos.victormoreno.algoritmos;
 
+/**
+ * 
+ * @author victor
+ * @version 1.0
+ *
+ */
+
 public abstract class algoritmos {
 
-	public algoritmos() {
-		
-	}
+	/**
+	 * 
+	 * @param numero
+	 * @return
+	 */
 	
 	public static int fibonacci(int numero) {
 		if(numero < 2) return numero;
 		return fibonacci(numero-1) + fibonacci(numero-2);
 	}
 	
+	public static int factorial(int numero) {
+		int factorial = 1;
+		for(int i = 2; i <= numero; i++) {
+			factorial *= i;
+		}
+		return factorial;
+	}
 }
