ae86ef9a35a250449594593b3836a8217ba794a9
Victor <vmb0003@alu.medac.es>
2023-03-27 09:43:06 +0200

    Metodo fibonacci

--- com.entornos.victormoreno.algoritmos/src/com/entornos/victormoreno/algoritmos/algoritmos.java
+++ com.entornos.victormoreno.algoritmos/src/com/entornos/victormoreno/algoritmos/algoritmos.java
@@ -2,4 +2,13 @@ package com.entornos.victormoreno.algoritmos;
 
 public abstract class algoritmos {
 
+	public algoritmos() {
+		
+	}
+	
+	public static int fibonacci(int numero) {
+		if(numero < 2) return numero;
+		return fibonacci(numero-1) + fibonacci(numero-2);
+	}
+	
 }
