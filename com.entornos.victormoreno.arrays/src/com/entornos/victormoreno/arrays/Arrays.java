81e474778ecd5a57c2f78c5dffb25cde4ddc1e7f
Victor <vmb0003@alu.medac.es>
2023-04-17 10:28:57 +0200

    Implemento la mediana

--- com.entornos.victormoreno.arrays/src/com/entornos/victormoreno/arrays/Arrays.java
+++ com.entornos.victormoreno.arrays/src/com/entornos/victormoreno/arrays/Arrays.java
@@ -10,6 +10,16 @@ public abstract class Arrays {
 		return suma/m.length;
 	}
 	
-	
+	public static int mediana(int[] m) {
+		int mediana;
+		java.util.Arrays.sort(m);
+		
+		if(m.length%2 == 0) {
+			mediana = m[m.length/2] + m[(m.length)-1];
+		} else {
+			mediana = m[((int)m.length/2)];
+		}
+		return mediana;
+	}
 	
 }
